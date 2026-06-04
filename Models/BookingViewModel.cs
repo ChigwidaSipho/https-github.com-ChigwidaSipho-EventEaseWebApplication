972daf49dab16dd3fdefe4c65417868141ee3b3e
ChigwidaSipho <st10488110@imconnect.edu.ZA>
2026-06-04 15:09:48 +0200

    Migrate schema and data to Azure SQL Database

--- Models/BookingViewModel.cs
+++ Models/BookingViewModel.cs
@@ -9,8 +9,8 @@
         public string EventName { get; set; }
         public DateTime EventDate { get; set; }
 
-        public int VenueID { get; set; }
-        public string VenueName { get; set; }
+        public int VenueID { get; set; }    
+        public string VenueName { get; set; } 
         public string Location { get; set; }
         public int Capacity { get; set; }
     }
