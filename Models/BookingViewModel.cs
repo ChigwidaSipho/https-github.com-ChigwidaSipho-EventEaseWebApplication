b9d6bcd9a8b7501db2a77e9db1d0348434f6361c
ChigwidaSipho <st10488110@imconnect.edu.ZA>
2026-06-04 15:11:01 +0200

    Connect application to live Azure Blob Storage account

--- Models/BookingViewModel.cs
+++ Models/BookingViewModel.cs
@@ -7,8 +7,8 @@
 
         public int EventID { get; set; }
         public string EventName { get; set; }
-        public DateTime EventDate { get; set; }
-
+        public DateTime EventDate { get; set; }             
+         
         public int VenueID { get; set; }    
         public string VenueName { get; set; } 
         public string Location { get; set; }
